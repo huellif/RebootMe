397a15207b5df515fc8e577e1e896b38566d81e0
huellif <huellif@gmail.com>
2013-12-24 17:14:53 +0100

    small comment improvments

--- main.cpp
+++ main.cpp
@@ -1,37 +1,40 @@
 #include <aknglobalmsgquery.h>
 
-class RestartSys //found in Symbian^3 sources
-    {
+class RestartSys //found in Symbian^3 sources, link against custrestartsys.lib
+{
 public:
     IMPORT_C static TInt RestartSystem();
-    };
+
+};
 
 LOCAL_C TAny ExeMainL() //TAny is Symbian's void, however compilers also accept void for native code
 {
-    CAknGlobalMsgQuery * pDlg = CAknGlobalMsgQuery::NewL();
-    CleanupStack::PushL(pDlg);
-    TRequestStatus iStatus;
-    pDlg->ShowMsgQueryL(iStatus,                    //the app should wait until the user selected an options
+    CAknGlobalMsgQuery * pDlg = CAknGlobalMsgQuery::NewL();//creating the pointer
+    CleanupStack::PushL(pDlg);                      //exception handling
+    TRequestStatus iStatus;                         //the app should wait until the user selected an option
+    pDlg->ShowMsgQueryL(iStatus,                    //the app should wait until the user selected an option
                         _L("Do you want to reboot your phone?\nPlease save all your documents!"),
-                        R_AVKON_SOFTKEYS_YES_NO,    //dialog buttons from avkon.rsg
+                        R_AVKON_SOFTKEYS_YES_NO,    //yes/no dialog buttons from avkon.rsg
                         _L("RebootMe"),             //dialog title
                         KNullDesC,                  //no image
                         0,                          //no icon
                         -1,                         //no icon
                         CAknQueryDialog::ENoTone);  //no sound
-    User::WaitForRequest(iStatus);
-    CleanupStack::PopAndDestroy(pDlg);
-    if (iStatus.Int() == EAknSoftkeyYes)
+
+    User::WaitForRequest(iStatus);                  //the app should wait until the user selected an option
+
+    CleanupStack::PopAndDestroy(pDlg);              //freeing CleanupStack
+    if (iStatus.Int() == EAknSoftkeyYes)            //only if user selected yes
     {
-        RestartSys::RestartSystem();
+        RestartSys::RestartSystem();                //reboot the device
     }
 }
 
 GLDEF_C TInt E32Main()
 {
-    CTrapCleanup* cl=CTrapCleanup::New(); //needed to use CleanupStack
-    TRAPD(error, ExeMainL()); //TRAPD is like a C++ try, the error code will be stored in TInt error
-    delete cl; //let's stay clean ;)
+    CTrapCleanup* cl=CTrapCleanup::New();           //needed to use CleanupStack
+    TRAPD(error, ExeMainL());                       //TRAPD is like a standart C++ try, the error code will be stored in TInt error
+    delete cl;                                      //let's stay clean
     return error;
 }
 
